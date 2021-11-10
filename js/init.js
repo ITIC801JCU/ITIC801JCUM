be753b4aec652a63302e948ed8e9e980103b20a3
ITIC801JCU <94069642+ITIC801JCU@users.noreply.github.com>
2021-11-10 12:24:06 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,4 +1,7 @@
-const firebaseConfig = {
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
   apiKey: "AIzaSyDiHfzM4LCXa6HfxMkk1G8JKTAn3-8CfoU",
   authDomain: "iticfire801jcu.firebaseapp.com",
   projectId: "iticfire801jcu",
@@ -6,4 +9,4 @@ const firebaseConfig = {
   messagingSenderId: "887933760440",
   appId: "1:887933760440:web:aa267c0e003c28699d154d",
   measurementId: "G-H3F308Y0J0"
-};
+    });
