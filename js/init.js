b58ecdf0d553eb06fb3997798277a52a5d3bdfac
ITIC801JCU <94069642+ITIC801JCU@users.noreply.github.com>
2021-11-10 12:06:55 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,7 +1,12 @@
-/*  Initializa Firebase con la configuración del proyecto. Revisa la
- * configuración en tu servidor de Firebase.  */
-// @ts-ignore
-firebase.initializeApp({
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyDiHfzM4LCXa6HfxMkk1G8JKTAn3-8CfoU",
   authDomain: "iticfire801jcu.firebaseapp.com",
   projectId: "iticfire801jcu",
@@ -9,4 +14,8 @@ firebase.initializeApp({
   messagingSenderId: "887933760440",
   appId: "1:887933760440:web:aa267c0e003c28699d154d",
   measurementId: "G-H3F308Y0J0"
-    });
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
